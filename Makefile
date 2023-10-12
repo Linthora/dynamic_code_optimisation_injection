8935420d97661561e331c25b96820cc68b79aba1
linthora <linthora@pop-os.localdomain>
2023-10-12 12:53:29 +0200

    modified makefile and updated a little prog.c

---
+++ Makefile
@@ -0,0 +1,25 @@
+# rm and create build directory and then compile an executable from src_main/prog.c
+
+# Variables
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -pedantic -std=c99 -g
+SRC = src_main/prog.c
+OBJ = $(SRC:.c=.o)
+EXEC = prog
+BUILD_DIR = build
+
+# Rules
+all: $(EXEC)
+
+$(EXEC): $(OBJ)
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CFLAGS) -o $(BUILD_DIR)/$@ $^
+
+%.o: %.c
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+clean:
+	rm -rf $(BUILD_DIR)
+
+run:
+	./$(BUILD_DIR)/$(EXEC)
--- src_main/prog.c
+++ src_main/prog.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 
 
 
 // à opti en live en speed_ext
-int exp(long long x, long long y) {
+long long exp_here(long long x, long long y) {
 
     if(y < 0) {
         printf("Error: y must be positive\n");
@@ -20,7 +21,7 @@ int exp(long long x, long long y) {
 }
 
 void answer() {
-    printf("Hello, exp(10,30) = %ll\n", exp(10, 30));
+    printf("Hello, exp(3,100) = %lli\n", exp_here(3, 10000));
 }
 
 // idée -> csv pour indiquer le temps pris par chaque itération et plot le résultat. (python)
@@ -28,6 +29,7 @@ int main(int argc, char *argv[]) {
     int nb_iter = 50000;
     
     if(argc > 1) {
+
         nb_iter = atoi(argv[1]);
     }
 
@@ -39,8 +41,8 @@ int main(int argc, char *argv[]) {
         answer();
         stop = clock();
         float time_taken = ((float)stop - (float)start) / CLOCKS_PER_SEC;
-        printf("Time taken: %f\n", time_taken);
         data_time_taken[i] = time_taken;
+        printf("Time taken: %f\n", data_time_taken[i]);
     }
     // write in csv: TODO latter
     
